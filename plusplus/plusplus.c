738ffb4e1e91b3e108cf65bf429fcf24878906ca
Rick Baker <chrissynrick@yahoo.com>
2025-04-25 12:26:30 -0700

    Added plusplus.c

---
+++ plusplus/plusplus.c
@@ -0,0 +1,13 @@
+/***************************************************************************/
+/* plusplus.c code snippet from C With Assembly Language by Steven Holzner */
+/* Pages 31-32                                                             */
+/***************************************************************************/
+#include <stdio.h>
+
+main()
+{
+    int i = 12, j = 12;
+
+    printf("i, i++ = %d, %d\n", i, i++);
+    printf("j, ++j = %d, %d\n", j++, j);
+}
