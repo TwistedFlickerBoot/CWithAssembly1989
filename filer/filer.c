3d66658603fa836b2db35cc61ef4641102304930
Rick Baker <chrissynrick@yahoo.com>
2025-04-26 17:54:09 -0700

    Fixing an issue with auto completion

--- filer/filer.c
+++ filer/filer.c
@@ -3,6 +3,9 @@
 /* C With Assembly by Steven Holzner - A Brady Book                                          */
 /* Typed by Rick/MerlinTheSpook/TwistedFlickerBoot/SchematicRick                             */
 /*********************************************************************************************/
+/* UPDATING ALONG THE CHAPTER, GIT SHOULD HAVE THE HISTORY BUT I WILL ALSO COMMENT OUT       */
+/* INSTEAD OF DELETING                                                                       */
+/******************************************************************************************* */
 #include <stdio.h>
 
 main()
@@ -10,7 +13,7 @@ main()
     FILE *file_pointer;
     int num;
 
-    if((file_pointer = fopen("file.txt", "W")) == NULL) {
+    if((file_pointer = fopen("number.dat", "w")) == NULL) {
         printf("Type some integers: \n");
         while(scanf("%d", &num) == 1) fprintf(file_pointer, "%d\n", num);
         fclose(file_pointer);
